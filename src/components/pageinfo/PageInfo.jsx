95520db5730fc883cf471d7c062385dcbcca1a2a
Matthew Campbell <matthewgcampbell@icloud.com>
2025-10-18 01:55:00 -0400

    Update PageInfo UI for Google sign-in
    Changed the heading to 'Sign In With Google', added an extra text input, and introduced a 'Sign In' button to enhance the sign-in interface.

--- src/components/pageinfo/PageInfo.jsx
+++ src/components/pageinfo/PageInfo.jsx
@@ -4,8 +4,10 @@ import styles from './pageinfo.module.css';
 function PageInfo() {
   return (
     <div className={styles.info_container}>
-        <h1>Sign in</h1>
+        <h1>Sign In With Google</h1>
         <input type="text"/>
+        <input type="text"/>
+        <button>Sign In</button>
     </div>
   )
 }
