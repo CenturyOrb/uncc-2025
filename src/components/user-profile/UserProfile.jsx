c6bb495e4d395ad5f529b07d0b6d9c4bbaefbbf8
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-10-19 04:15:22 -0400

    Made UserProfile take in db data

--- src/components/user-profile/UserProfile.jsx
+++ src/components/user-profile/UserProfile.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from 'react'
 import styles from './userprofile.module.css'
 import bob from '../../assets/bobross.jpeg'
 import { FiArrowUp } from "react-icons/fi";
 
+import axios from 'axios'
+
 const UserProfile = () => { 
+	const [userProfile, setUserProfile] = useState({});	
+
+	useEffect(() => { 
+		// call backend for user info /users/:id
+		const fetchData = async () => {
+      		try {
+        		const response = await axios(`https://reviewless-mallie-conchal.ngrok-free.dev/users/1AYFBciXLCgQqhFH7UlNMn5TMjn2`);
+				setUserProfile(response.data.user);
+    		} catch (error) {
+    		    console.error('Error fetching data:', error);
+    		}
+    	};		
+		fetchData();
+	}, []);		
+	
 	return(
 		<div className={styles.user_profile}>
 			<img 
@@ -14,15 +32,15 @@ const UserProfile = () => {
 					style={{display: 'flex', alignItems: 'center'}}
 					className={styles.story}
 				>
-					Bobby Rossy <span style={{marginLeft: 'auto', fontSize: '1.1rem'}}>
+					{userProfile.user_name}<span style={{marginLeft: 'auto', fontSize: '1.1rem'}}>
 					he/him</span>
 				</p>
 				<hr/>
 				<div className={styles.user_body}>
-					<p className={styles.user_info}>hello goobers </p>
+					<p className={styles.user_info}>{userProfile.user_description}</p>
 					<span style={{display: 'flex', alignItems: 'center', gap: '0.5rem'}}>
 						<FiArrowUp size={24}/>
-						4858
+						{userProfile.upvote_count}
 					</span>
 				</div>
 			</div>
