4dd5c34442f1dc5ae892cf2d9c40264e8c7e9894
Matthew Campbell <matthewgcampbell@icloud.com>
2025-10-18 22:20:31 -0400

    Add onboarding modal and clean up header navigation
    Introduced OnboaringModal component and its CSS for onboarding UI. Updated LandingPage to include the onboarding modal. Removed unused navigation buttons from Header. Minor formatting and logging improvements in ActivityGrid.

--- src/pages/landing-page/LandingPage.jsx
+++ src/pages/landing-page/LandingPage.jsx
@@ -3,29 +3,10 @@ import Content from "../../components/images/Images.jsx";
 import "./LandingPage.css";
 import ChatBox from "../../components/chat-box/ChatBox.jsx";
 import Images from "../../components/images/Images.jsx";
-import Title from "../../components/title-component/title.jsx";
 import OnboardingModal from "../../components/onboaring-modal/OnboaringModal.jsx";
+import Title from "../../components/title-component/title.jsx";
 
 const LandingPage = () => {
-	return (
-		<div className="landingpage_container">
-			<Header />
-			<div className="content_wrapper">
-				<Images />
-        <div className="box_overlay">
-			<Title />	
-          	<ChatBox />
-=======
-import Title from "../../components/title-component/title.jsx";
-import OnboardingModal from "../../components/onboaring-modal/OnboaringModal.jsx";
-
-const LandingPage = () => {
-=======
-import Title from "../../components/title-component/title.jsx";
-import OnboardingModal from "../../components/onboaring-modal/OnboaringModal.jsx";
-
-const LandingPage = () => {
->>>>>>> Stashed changes
   return (
     <>
       <OnboardingModal />
@@ -37,10 +18,6 @@ const LandingPage = () => {
             <Title />
             <ChatBox />
           </div>
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         </div>
       </div>
     </>
