1010766abbaad77242a7d0dea024b02c806fb37f
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2025-10-19 08:37:16 -0400

    started markdown

---
+++ src/components/markdown-viewer/MarkdownViewer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styles from './markdownviewer.module.css'; 
+
+const MarkdownViewer = ({ content }) => {
+  return (
+    <div className={`${styles.markdownContainer} ${styles.test}`}>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  );
+};
+
+export default MarkdownViewer;
